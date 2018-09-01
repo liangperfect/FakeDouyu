7ca524d056c10fdcec71b6a1604e65a0943d241f
chenliang0408 <chenliang0408@thundersoft.com>
2018-09-01 14:01:33 +0800

    [common] Project add 3rf library

---
+++ app/src/main/java/com/fakedouyu/user/fakedouyu/FlashingActivity.java
@@ -0,0 +1,19 @@
+package com.fakedouyu.user.fakedouyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashing);
+        
+
+    }
+
+
+
+
+}
